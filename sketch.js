f8e1b64e1dcc29cb232827da1fd002d65da0ace3
shivam54321 <59958317+shivam54321@users.noreply.github.com>
2020-03-25 17:24:27 +0530

    c-35 code

---
+++ sketch.js
@@ -0,0 +1,24 @@
+
+function setup(){
+    database = firebase.database();
+    console.log(database);
+    createCanvas(500,500);
+
+    game = new Game();
+    console.log(game.getState());
+   // game.start();
+
+   
+}
+
+function draw(){
+
+    
+}
+
+
+
+
+
+
+
